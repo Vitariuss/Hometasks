799e16e4d6247eb19212e7957c7a4b0a552f0f11
Vit <vitarius@gmail.com>
2023-02-03 15:13:53 +0800

    done

--- Sem2.5/Program.cs
+++ Sem2.5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Write down your first number");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write down your second number");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Yes, one of these numbers is a square of another one");
+}
+else
+{
+    Console.WriteLine("No, neither of these numbers is a square of another one");
+}
