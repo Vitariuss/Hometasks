26d03f5fcd2e13df7b23b55bd412d4f1f1ce13a8
Vit <vitarius@gmail.com>
2023-02-06 14:07:56 +0800

    done

--- Sem5.1/Program.cs
+++ Sem5.1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int[] generateArray(int length, int start, int end)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+void printArray(int[] array)
+{
+    Console.WriteLine("[")
+    
+}
---
+++ Sem5Dom1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.WriteLine("How big should your array be?");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Here is your array: ");
+PrintArray(numbers);
+int even = 0;
+
+for (int num = 0; num < numbers.Length; num++)
+if (numbers[num] % 2 == 0)
+even++;
+
+Console.WriteLine($"There are {numbers.Length} numbers in your array and only {even} of them are even ones");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
