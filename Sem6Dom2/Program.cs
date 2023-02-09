5da08664f9ea6092c8da27376f35646d2b773eb7
Vit <vitarius@gmail.com>
2023-02-09 22:37:03 +0800

    changed

--- Sem6Dom2/Program.cs
+++ Sem6Dom2/Program.cs
@@ -1,16 +1,16 @@
 ﻿Console.Clear();
 
-Console.Write("Write k1 ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Write b1 ");
+Console.Write("Write b1 - ");
 double b1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Write k2 ");
-double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Write k1 - ");
+double k1 = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Write b2 ");
+Console.Write("Write b2 - ");
 double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Write k2 - ");
+double k2 = Convert.ToDouble(Console.ReadLine());
 
 double x = -(b1 - b2) / (k1 - k2);
 double y = k1 * x + b1;
