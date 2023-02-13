05adc7a7ede14bd808f4d9a188e02c4d602289cc
Vit <vitarius@gmail.com>
2023-02-13 19:28:55 +0800

    changed

---
+++ Sem7Dom1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+    FillArray();
+    Console.Write(""); Console.ReadLine();
+
+    void FillArray()
+    {
+        Console.WriteLine("Write the size of your array m x n: ");
+        Console.Write("Write the number of lines" + "  " + "m = ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Write the number of columns" + "  " + "n = ");
+        int n = Convert.ToInt32(Console.ReadLine()); 
+
+        double[,] array = new double[m, n];
+        Random random = new Random();
+        Console.WriteLine();
+        Console.WriteLine("Here is your array!");
+        Console.WriteLine();
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                array[i, j] = Math.Round((random.NextDouble() * 200 - 100), 2);
+                Console.Write("{0, 10}", (array[i, j]).ToString("0.0"));
+               
+            }
+        Console.WriteLine();
+        }
+    }
+  
