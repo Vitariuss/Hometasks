d2fa95c282eeff527671a77822294ae698332027
Vit <vitarius@gmail.com>
2023-02-20 20:42:00 +0800

    changed

---
+++ Sem9Dom1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Write your number");
+int n = int.Parse(Console.ReadLine());
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Wrong number");
+}
+Console.WriteLine(NatNum(n, m));
+
+int NatNum(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NatNum(n, m + 1)}, ");
+    return m;
+}
+
