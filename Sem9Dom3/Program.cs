59964200fa467a10840828f28b4beb5915253f72
Vit <vitarius@gmail.com>
2023-02-20 22:25:11 +0800

    changed

---
+++ Sem9Dom3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("You need to inout n and m numbers in Ackermann function (n, m)");
+Console.WriteLine("Write the n number");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Write the m number");
+int m = int.Parse(Console.ReadLine());
+
+int Ack(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Ack(n - 1, 1);
+    else
+        return Ack(n - 1, Ack(n, m - 1));
+}
+ 
+Console.WriteLine($"The result of Ackerman function ({n}, {m}) is {Ack(n, m)}");
+Console.ReadKey();
