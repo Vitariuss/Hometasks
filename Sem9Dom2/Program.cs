a6e3d1378153b20060b5e1c11ab6a00302ba6ba2
Vit <vitarius@gmail.com>
2023-02-20 21:20:06 +0800

    changed

---
+++ Sem9Dom2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Write your first number (less one)");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Write your second number (bigger one)");
+int m = int.Parse(Console.ReadLine());
+
+
+int CountSum(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        return n + CountSum(n, m-1);
+}
+
+
+Console.WriteLine($"Sum of the numbers from {n} to {m} is {CountSum(n, m)}");
