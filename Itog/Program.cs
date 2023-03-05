87ef655ba05361f893427fd4b0bebf8f13cfe4f2
Vit <vitarius@gmail.com>
2023-03-05 15:00:56 +0800

    done

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите слова или числа через пробел");
+string str = Console.ReadLine();
+string[] array = str.Split(' ').Distinct().ToArray();
+
+var array2 = array.Where(x=>x.Length < 4).ToArray();
+
